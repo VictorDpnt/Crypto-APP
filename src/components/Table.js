58d41d8274b11938687387595029423ed8db4e0d
VictorDpnt <157797158+VictorDpnt@users.noreply.github.com>
2024-02-29 16:50:29 +0100

    Crypto App v1

---
+++ src/components/Table.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import TableLine from "./TableLine";
+import ToTop from "./ToTop";
+
+const Table = ({ coinsData }) => {
+  const [rangeNumber, setRangeNumber] = useState(100);
+  const [orderBy, setOrderBy] = useState("");
+  const tableHeader = [
+    "Prix",
+    "MarketCap",
+    "Volume",
+    "1h",
+    "1j",
+    "1s",
+    "1m",
+    "6m",
+    "1a",
+    "ATH",
+  ];
+  return (
+    <div className="table-container">
+      <div className="table-header">
+        <div className="range-container">
+          <span>
+            Top{" "}
+            <input
+              type="text"
+              value={rangeNumber}
+              onChange={(e) => setRangeNumber(e.target.value)}
+            />
+          </span>
+          <input
+            type="range"
+            min="1"
+            max="250"
+            value={rangeNumber}
+            onChange={(e) => setRangeNumber(e.target.value)}
+          />
+          <ToTop />
+        </div>
+        {tableHeader.map((el) => {
+          return (
+            <li key={el}>
+              <input
+                type="radio"
+                name="header-el"
+                id={el}
+                defaultChecked={
+                  el === orderBy || el === orderBy + "reverse" ? true : false
+                }
+                onClick={() => {
+                  if (orderBy === el) {
+                    setOrderBy(el + "reverse");
+                  } else {
+                    setOrderBy(el);
+                  }
+                }}
+              />
+              <label htmlFor={el}>{el}</label>
+            </li>
+          );
+        })}
+      </div>
+      {coinsData &&
+        coinsData.slice(0, rangeNumber).map((coin, index) => {
+          return <TableLine coin={coin} index={index} />;
+        })}
+    </div>
+  );
+};
+
+export default Table;
