1b1fbf2a3eaaf93d9912793d54f30c526b66f9ec
VictorDpnt <157797158+VictorDpnt@users.noreply.github.com>
2024-03-04 15:17:44 +0100

    Crypto APP final

--- src/reducer/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import stableReducer from "./stableReducer";
-import listReducer from "./liste.reducer";
-export default combineReducers({
-  stableReducer,
-  listReducer,
-});
